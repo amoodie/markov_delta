1cf82c372c0b38724d39ad4d0bfdd1f9ed4ab73e
Kelly Sanks <kmsanks@uark.edu>
2018-07-30 13:09:45 -0500

    Added change in elevation code

--- src/preprocess.m
+++ src/preprocess.m
@@ -1,1 +1,32 @@
 % in this code we do any manipulation of the data needed before any other codes
+sl=(1:900)*0.25;
+time=2:900;
+topo=permute(z(time,:,:),[2 3 1]);
+topoprev=permute(z(time-1,:,:),[2 3 1]);
+dtopo=topo-topoprev;
+dtopo2=dtopo;
+RSLR=0.25-dtopo2;
+
+dz = z(2:end, :,:)-z(1:end-1,:,:);
+histogram(dz(:))
+sl=(1:900)*0.25;
+time = 2:900;
+
+for i=2:900;
+    x= sl(i);
+    topo = permute(z(i,:,:),[2 3 1]);
+    topo(topo(:)<=x)=NaN;
+    toposave(i,:,:)=permute(topo,[3,1,2]);
+end
+    %topo=permute(z(i,:,:),[2 3 1]);
+    %(topo(:)==0)=NaN;
+    %marsh is anything that is less than sea level and greater than 10mm
+    %below sea level
+    %strat= double(topo<=x);
+    %topo(topo <=x) = NaN;
+    %strat =(topo(:)==0)=NaN;
+
+
+
+dz = z(2:end, :, :)-z(1:end-1,:,:);
+histogram(dz(:))
