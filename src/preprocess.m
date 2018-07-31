34dcd75b760ee0aae366d8da4f2b4f958335b4b0
Andrew Moodie <andmoo22@gmail.com>
2018-07-30 22:39:44 -0500

    rework the function to account for all timesteps quickly, and add a plotting function

--- src/preprocess.m
+++ src/preprocess.m
@@ -1,32 +1,35 @@
 % in this code we do any manipulation of the data needed before any other codes
 
-z = load(fullfile('..', 'data', 'TDB_12_Dry_z.mat'))
+z = load(fullfile('..', 'data', 'TDB_12_Dry_z.mat'));
+z = z.z; % 900 = t, 455 = x, 391 = y
 
-sl=(1:900)*0.25;
-time=2:900;
-topo=permute(z(time,:,:),[2 3 1]);
-topoprev=permute(z(time-1,:,:),[2 3 1]);
-dtopo=topo-topoprev;
-dtopo2=dtopo;
-RSLR=0.25-dtopo2;
+sl = (1:900) .* 0.25; % sea level at each time t
+% time = 2:900; % shouldn't time start at 0 and we index it beyond that?
+% topo = permute(z(time, :, :), [2, 3, 1]);
+% topoprev = permute(z(time-1, :, :),[2, 3, 1]);
+% dtopo = topo - topoprev;
+% dtopo2 = dtopo;
+% RSLR = 0.25 - dtopo2;
+% 
+% for i = 2:900
+%     x = sl(i);
+%     topo = permute(z(i,:,:),[2 3 1]);
+%     topo(topo(:)<=x)=NaN;
+%     toposave(i,:,:)=permute(topo,[3,1,2]);
+% end
 
-
-
-for i=2:900;
-    x= sl(i);
-    topo = permute(z(i,:,:),[2 3 1]);
-    topo(topo(:)<=x)=NaN;
-    toposave(i,:,:)=permute(topo,[3,1,2]);
+T = 0:900; % one element longer than scans
+for t = T(2:end)
+    slt = sl(t); % sea level at time t
+    mart = squeeze( z(t, :, :) < slt ); % what is marine (below sl) at t
+    z(t, mart) = NaN; % replace at identified indicies
 end
-    %topo=permute(z(i,:,:),[2 3 1]);
-    %(topo(:)==0)=NaN;
-    %marsh is anything that is less than sea level and greater than 10mm
-    %below sea level
-    %strat= double(topo<=x);
-    %topo(topo <=x) = NaN;
-    %strat =(topo(:)==0)=NaN;
 
+dz = z(2:end, :, :) - z(1:end-1, :, :); % vectorized dz
 
+nbins = 9;
 
-dz = z(2:end, :, :)-z(1:end-1,:,:);
-histogram(dz(:))
+figure()
+histogram(dz(:), nbins);
+
+[hc] = histcounts(dz(:, nbins);
