59ce6aa4169d8f1c124c7033e62d3812f7ddb4e4
Andrew Moodie <andmoo22@gmail.com>
2018-07-31 08:30:25 -0500

    working prob table

--- src/markov_matrix.m
+++ src/markov_matrix.m
@@ -10,15 +10,18 @@ dz = dzs.dzs.dz; %
 be = dzs.dzs.be; % bin edges
 hc = dzs.dzs.hc;
 
-bc = cumsum( (be(2:end) - be(1:end-1)) / 2 ); % bin centers
+bc = be(1) + cumsum( (be(2:end) - be(1:end-1)) / 2 ); % bin centers
 
 markov_mat = zeros(length(hc)); % rows = to, cols = from
+neldz = numel(dz);
 
 %% loop to determine next state for given bin
 for s = 1:length(be)-1
     hcs = bc(s); % current state bin
-    sidx = and(dz(1:end-1, :, :) >= be(s), dz(1:end-1, :, :) < be(s+1)); % indicies at current state
-    dzsp1 = dz(find(sidx)+1); % dz value for state plus 1
+    sidx = and(dz >= be(s), dz < be(s+1)); % indicies at current state
+    sidxp1 = find(sidx)+1;
+    sidxp1 = sidxp1(sidxp1 <= neldz);
+    dzsp1 = dz(sidxp1); % dz value for state plus 1
     hcs = histcounts(dzsp1, be); % hist counts
     nhcs = hcs ./ sum(hcs); % normalized histcounts
     markov_mat(:, s) = nhcs'; % into storage
