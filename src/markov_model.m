5689dca453233145075d43ad3e1ce97ed50a41b6
Andrew Moodie <andmoo22@gmail.com>
2018-07-31 13:31:21 -0500

    add note for how to do the looping

--- src/markov_model.m
+++ src/markov_model.m
@@ -1,14 +1,20 @@
 % in this code we evaluate the markov model in a single stratigraphic column
 
-%% make pdfs from deposit data
-
-
-
-%% make matrix of probabilities
-
 
 %% evaluate the model 1 time
 RSLR = 0.25; % need to make sure to add RSLR on each step to make a net aggradation
+BL = 0; % base level
+z = 0; % random value for initial from within interval
+for t = 1:900
+    % look at column index for current z
+    % generate a random number in interval [0 1]
+    % determine which prob bin it falls in to
+    % go that far in z
+    % as RSLR in z
+    
+    
+end
+
 
 
 %% evaluate 1000 times and compute stats
