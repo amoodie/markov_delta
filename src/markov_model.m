b786ce08fba9c1b685af49a2e400498d404f7806
Andrew Moodie <andmoo22@gmail.com>
2018-08-01 08:33:06 -0500

    working model, need to debug

--- src/markov_model.m
+++ src/markov_model.m
@@ -1,33 +1,59 @@
 % in this code we evaluate the markov model in a single stratigraphic column
 
+clear variables
+
 %% load vars
 disp('loading...')
-dzs = load(fullfile('..', 'data', 'markov_mat.mat'));
-be = mm.be; % bin edges
-bc = mm.bc; % bin centers
-mm = mm.mm; % markov matrix
+mm = load(fullfile('..', 'data', 'markov_mat.mat'));
+be = mm.mm.be; % bin edges
+bc = mm.mm.bc; % bin centers
+mm = mm.mm.mm; % markov matrix
 
 %% aux vars
 beidx = cumsum(be);
-mmcs = cumsum(mm, 1);
+mmcs = cumsum(mm, 1, 'reverse');
+
+T = 1:900;
+zs = [];
+
+% figure()
+% [x, y] = meshgrid(bc, bc);
+% surface(x, y, mmcs, 'EdgeColor', 'k')
+
 
 %% evaluate the model 1 time
 RSLR = 0.25; % need to make sure to add RSLR on each step to make a net aggradation
 BL = 0; % base level
 z = 0; 
 dz = 0; % random value for initial from within interval
-for t = 1:900
-    cidx = find(dz > be, 1, 'first') - 1; % first greater than minus one is idx
+
+figure()
+
+for t = T
+    % store
+    zs(t) = z;
+    ts(t) = t;
+    
+    % find the bin
+    cidx = find(dz < be, 1, 'first') - 1; % first greater than minus one is idx
 
     % generate a random number in interval [0 1]
-    rand(1)
+    newrand = rand(1);
 
     % determine which prob bin it falls in to
+    dzidx = find(mmcs(:, cidx) < newrand, 1, 'first');
+    %% ERRORS WHEN dz < first bin
+    dz = be(dzidx);
     
     % go that far in z
+    z = z + dz;
     
-    % as RSLR in z
+    % add RSLR in z
+    z = z + RSLR;
     
+    % plot
+    stairs(ts, zs)
+    drawnow
     
 end
 
