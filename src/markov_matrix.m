1f2ef8ecc9bb5b1304faf2606572d7c51704ee3f
Andrew Moodie <andmoo22@gmail.com>
2018-07-31 22:36:44 -0500

    swap file load and some markov_mat dev

--- src/markov_matrix.m
+++ src/markov_matrix.m
@@ -21,16 +21,18 @@ disp('determining matrix...')
 for s = 1:length(bc)
 %     hcs = bc(s); % current state bin
     sidx = and(dzp >= be(s), dzp < be(s+1)); % indicies at current state
-    sidxp1 = find(sidx) + numel(dzp(:, :, 1)); % index one slice below
-    sidxp1 = sidxp1(sidxp1 <= neldz);
+    sidxp1a = find(sidx) + numel(dzp(:, :, 1)); % index one slice below
+    sidxp1 = sidxp1a(sidxp1a <= neldz);
     dzsp1 = dz(sidxp1); % dz value for state plus 1
     hcs = histcounts(dzsp1, be); % hist counts
     nhcs = hcs ./ sum(hcs); % normalized histcounts
     markov_mat(:, s) = nhcs'; % into storage
 end
 
+%% plot the matrix
 figure()
 [x, y] = meshgrid(bc, bc);
 surface(x, y, markov_mat, 'EdgeColor', 'k')
 % image(markov_mat)
 
+j=1;
